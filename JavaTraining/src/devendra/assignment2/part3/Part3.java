18900c464f03567e4621ff543bfbb1b75203a562
Devenc042 <30405936+Devenc042@users.noreply.github.com>
2017-07-31 12:26:04 +0530

    comment add to explain the answer.

--- JavaTraining/src/devendra/assignment2/part3/Part3.java
+++ JavaTraining/src/devendra/assignment2/part3/Part3.java
@@ -12,3 +12,8 @@ public class Part3 {
 	}
 
 }
+
+// if we do Part3 [] array=new Part3[10]; then no objects are created except your array, 
+// each field will be null until you initialize it. 
+// Above only create place to store reference of Part3 Class object. 
+// Although Array array is created but its not referencing any object and we can't do like array[i].someMethod(). 

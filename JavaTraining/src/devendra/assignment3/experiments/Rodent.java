6cea74a8ccd576d490821da708cd213e3ab84225
= <=>
2017-08-01 14:57:51 +0530

    assignment3, experiments commited

---
+++ JavaTraining/src/devendra/assignment3/experiments/Rodent.java
@@ -0,0 +1,56 @@
+//package devendra.assignment3.experiments;
+//
+//abstract class Rodent {
+//	Rodent(){
+//		System.out.println("Rodent was here");
+//	}	
+//	abstract void Eat();
+//	abstract void Sleep();
+//	abstract void Code();
+//}
+//
+// class Mouse extends Rodent {
+//	 
+//	 Mouse(){
+//		 System.out.println("Mouse was here");
+//	 }	 
+//	 void Eat() { System.out.println("Mouse likes to eat");}
+//	 void Sleep() { System.out.println("Mouse likes to sleep");}
+//	 void Code() { System.out.println("Mouse likes to code");}
+//}
+// 
+// class Gerbil extends Rodent {
+//	 
+//	 Gerbil(){
+//		 System.out.println("Gerbil was here");
+//	 }	 
+//	 void Eat() { System.out.println("Gerbil likes to eat");}
+//	 void Sleep() { System.out.println("Gerbil likes to sleep");}
+//	 void Code() { System.out.println("Gerbil likes to code");}
+//}
+// class Hamster extends Rodent {
+//	 
+//	 Hamster(){
+//		 System.out.println("Hamster was here");
+//	 }	 
+//	 void Eat() { System.out.println("Hamster likes to eat");}
+//	 void Sleep() { System.out.println("Hamster likes to sleep");}
+//	 void Code() { System.out.println("Hamster likes to code");}	
+//}
+// 
+// class TestRodent {
+//
+//		public static void main(String[] args) {
+//			// TODO Auto-generated method stub
+//			
+//			Rodent r;
+//			r = new Mouse();
+//			r.Eat();
+//			
+//			r = new Gerbil();
+//			r.Sleep();
+//			
+//			r = new Hamster();
+//			r.Code();
+//		}
+// }
---
+++ JavaTraining/src/devendra/assignment3/experiments/TestCycle.java
@@ -0,0 +1,10 @@
+package devendra.assignment3.experiments;
+
+public class TestCycle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
